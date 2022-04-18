f7b184e33b9310e5f2aacbc18c633c27cd73b53f
Anastasia <nsta_nikolaeva@inbox.ru>
2022-04-18 21:45:42 +0300

    Example11

---
+++ Example_010/Program.cs
@@ -0,0 +1,19 @@
+﻿// найти номер элемента массива, после того как пользователь введет сам элемент массива
+int []array = {1, 12, 31, 4, 15, 16, 4, 18};
+
+int n = array.Length;
+
+int find = 4;
+
+int index = 0;
+while (index < n)
+{
+    if (array [index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index+1
+    index++;
+}
+
